4d7fd73d27be40d655c740a38bed649e00aa36d7
green2113 <kangmw081122@gmail.com>
2026-04-25 22:05:30 +0900

    2.1.1 버전 업데이트

--- src/game/version.h
+++ src/game/version.h
@@ -22,12 +22,12 @@ extern const char *GIT_SHORTREV_HASH;
 
 // UClient
 #ifndef UCLIENT_VERSION
-#define UCLIENT_VERSION "2.1.0"
+#define UCLIENT_VERSION "2.1.1"
 #endif
 
 // TClient compatibility
 #ifndef TCLIENT_VERSION
-#define TCLIENT_VERSION "2.1.0"
+#define TCLIENT_VERSION "10.8.7"
 #endif
 
 // BestClient
