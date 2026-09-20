be2de7dc51bb368bed4fa79df8fb5dca633f0531
green2113 <kangmw081122@gmail.com>
2026-09-20 22:20:34 +0900

    Revert UClient and UClient Launcher versions to 2.10.5 and 1.2.3 respectively
    - Downgraded UClient version from "2.11.0" to "2.10.5" to align with previous stable release.
    - Downgraded UClient Launcher version from "1.3.0" to "1.2.3" for consistency with UClient versioning.
    
    These changes ensure version compatibility and maintain stability in the application.

--- src/game/version.h
+++ src/game/version.h
@@ -22,12 +22,12 @@ extern const char *GIT_SHORTREV_HASH;
 
 // UClient
 #ifndef UCLIENT_VERSION
-#define UCLIENT_VERSION "2.11.0"
+#define UCLIENT_VERSION "2.10.5"
 #endif
 
 // UClient Launcher
 #ifndef UCLIENT_LAUNCHER_VERSION
-#define UCLIENT_LAUNCHER_VERSION "1.3.0"
+#define UCLIENT_LAUNCHER_VERSION "1.2.3"
 #endif
 
 // TClient
