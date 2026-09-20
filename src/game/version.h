47e4b49b67f2deb68e444cd8f1d5031abe6bd60f
green2113 <kangmw081122@gmail.com>
2026-09-20 22:18:54 +0900

    Update UClient and UClient Launcher versions to 2.11.0 and 1.3.0 respectively
    - Incremented UClient version from "2.10.4" to "2.11.0" to reflect recent updates.
    - Updated UClient Launcher version from "1.2.2" to "1.3.0" to align with the latest changes.
    
    These updates ensure versioning accuracy and prepare for upcoming features.

--- src/game/version.h
+++ src/game/version.h
@@ -22,12 +22,12 @@ extern const char *GIT_SHORTREV_HASH;
 
 // UClient
 #ifndef UCLIENT_VERSION
-#define UCLIENT_VERSION "2.10.4"
+#define UCLIENT_VERSION "2.11.0"
 #endif
 
 // UClient Launcher
 #ifndef UCLIENT_LAUNCHER_VERSION
-#define UCLIENT_LAUNCHER_VERSION "1.2.2"
+#define UCLIENT_LAUNCHER_VERSION "1.3.0"
 #endif
 
 // TClient
